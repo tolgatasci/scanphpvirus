e330f7d3ba7ad6a8cd46e5fd0e7695838802eaa1
Tolga <tolgatasci1@gmail.com>
2022-05-05 17:22:07 +0300

    update

--- main.py
+++ main.py
@@ -150,14 +150,19 @@ def zip_run(args) -> None:
                         found_rules.append(Result(name=rule, found=found_line, line=index))
             file_.results = found_rules
 def last_run() -> None:
-
+    total_files = 0
+    found_danger = 0
     for item in data.directory:
+
         for file_ in item.files:
-            print(Fore.GREEN + "File: " + Style.RESET_ALL + file_.name)
+            total_files += 1
             if file_.results:
+                found_danger += 1
+                print(Fore.GREEN + "File: " + Style.RESET_ALL + item.name + "/" +file_.name)
                 for rulex in file_.results:
                     print(Fore.RED + "Rule: " + Style.RESET_ALL + rulex.name + Fore.RED + " found: "  + Style.RESET_ALL+ rulex.found  + Fore.RED+ " on line: " + Style.RESET_ALL + str(rulex.line))
-
+    print(Fore.GREEN + "Total files: " + Style.RESET_ALL + str(total_files))
+    print(Fore.RED + "Total found risk: " + Style.RESET_ALL + str(found_danger))
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Process some integers.')
